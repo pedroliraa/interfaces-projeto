9a31fad0fda0e616eeaab9debe04fda84a6fa97c
Letícia Gabrielly da Silva <165218856+LetGabs@users.noreply.github.com>
2026-04-09 14:29:07 -0300

    ajuste de merge

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,21 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-import AgendamentoList from "../pages/Agendamento/AgendamentoList";
-import ClienteList from "../pages/Cliente/ClienteList";
-=======
 import Login from "../pages/Auth/Login";
 import AgendamentoList from "../pages/Agendamento/AgendamentoList";
+import ClienteList from "../pages/Cliente/ClienteList";
 import PrivateRoute from "./PrivateRoute";
 import AdminLayout from "../layouts/AdminLayout";
->>>>>>> leticia
 
 function AppRoutes() {
   return (
     <Routes>
-<<<<<<< HEAD
-      <Route path="/admin/agendamentos" element={<AgendamentoList />} />
-      <Route path="/admin/clientes" element={<ClienteList />} />
-=======
       <Route path="/" element={<Login />} />
 
       <Route
@@ -28,13 +20,19 @@ function AppRoutes() {
           </PrivateRoute>
         }
       />
->>>>>>> leticia
+
+      <Route
+        path="/admin/clientes"
+        element={
+          <PrivateRoute>
+            <AdminLayout>
+              <ClienteList />
+            </AdminLayout>
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 }
 
-<<<<<<< HEAD
 export default AppRoutes;
-=======
-export default AppRoutes;
->>>>>>> leticia
