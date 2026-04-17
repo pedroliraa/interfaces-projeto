bbfd4478ec1adf91cf8a5b13eec748b90f2c57be
pedroliraa <pedrolirabandeira@gmail.com>
2026-04-16 22:26:51 -0300

    fix: corrige import do dotenv no api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: process.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
