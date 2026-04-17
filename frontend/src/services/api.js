97f5a38536e0f3906d8b9f06ba5b4a323bae3b62
pedroliraa <pedrolirabandeira@gmail.com>
2026-04-16 22:33:50 -0300

    fix: chama variable corretamente para o vite

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 
 const api = axios.create({
-  baseURL: process.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
